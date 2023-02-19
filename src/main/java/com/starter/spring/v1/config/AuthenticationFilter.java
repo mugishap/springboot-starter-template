0dbda710e01021b4176bc536a6b5e36cea6cf30e
mugishap <precieuxmugisha@gmail.com>
2023-02-19 08:09:09 +0200

    [init]: Initialize project

---
+++ src/main/java/com/starter/spring/v1/config/AuthenticationFilter.java
@@ -0,0 +1,23 @@
+package com.starter.spring.v1.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String userId;
+        final String jwtToken;
+        System.out.println();
+    }
+}
